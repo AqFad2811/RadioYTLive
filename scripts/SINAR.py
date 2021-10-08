a6af974eaa74db8835c644b76694baa8b28f6157
AqFad <85995579+AqFad2811@users.noreply.github.com>
2021-10-08 18:14:14 +0800

    Update and rename youtube_m3ugrabber9.py to SINAR.py

--- scripts/SINAR.py
+++ scripts/SINAR.py
@@ -40,7 +40,7 @@ print('#EXTM3U')
 print('#EXT-X-VERSION:3')
 print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
 s = requests.Session()
-with open('../youtube_channel_info9.txt') as f:
+with open('../sinar_info.txt') as f:
     for line in f:
         line = line.strip()
         if not line or line.startswith('~~'):
