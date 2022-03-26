240bce66f4565bbecd0235c895903ea1ff9182ab
AqFad <85995579+AqFad2811@users.noreply.github.com>
2022-03-26 18:16:24 +0800

    Update ERA.py

--- scripts/ERA.py
+++ scripts/ERA.py
@@ -13,12 +13,12 @@ def grab(url):
     response = s.get(url, timeout=15).text
     if '.m3u8' not in response:
         if windows:
-            print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u')
+            print('https://onedrive.aqfadtv.xyz/api/raw/?path=/harapmaaf.mp4')
             return
         os.system(f'wget {url} -O temp.txt')
         response = ''.join(open('temp.txt').readlines())
         if '.m3u8' not in response:
-            print('https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u')
+            print('https://onedrive.aqfadtv.xyz/api/raw/?path=/harapmaaf.mp4')
             return
     end = response.find('.m3u8') + 5
     tuner = 100
